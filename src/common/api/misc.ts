66b119f9610426d15651d531a5f4b7ec27a69181
Suncheol Kwon <elytselyts@gmail.com>
2022-07-27 14:25:54 +0900

    upload image

--- src/common/api/misc.ts
+++ src/common/api/misc.ts
@@ -4,27 +4,104 @@ import defaults from "../constants/defaults.json";
 
 import { apiBase } from "./helper";
 
-export const getEmojiData = () =>
-  fetch(apiBase("/emoji.json")).then((response) => response.json());
+export const getEmojiData = () => fetch(apiBase("/emoji.json")).then((response) => response.json());
 
 export const uploadImage = async (
   file: File,
-  token: string
+  username: string
 ): Promise<{
   url: string;
 }> => {
-  const fData = new FormData();
-  fData.append("file", file);
+  // const fData = new FormData();
+  // fData.append("file", file);
 
-  // const postUrl = `${defaults.imageServer}/hs/${token}`;
-  const postUrl = `${defaults.imageServer}/`;
-  return axios
-    .post(postUrl, fData, {
+  // // const postUrl = `${defaults.imageServer}/hs/${token}`;
+  // const postUrl = `${defaults.imageServer}/`;
+  // return axios
+  //   .post(postUrl, fData, {
+  //     headers: {
+  //       "Content-Type": "multipart/form-data",
+  //     },
+  //   })
+  //   .then((r) => r.data);
+
+  debugger;
+
+  const keychainLogin = true;
+
+  let data: any, dataBs64;
+
+  if (file) {
+    // drag and drop
+    const reader: any = new FileReader();
+    data = await new Promise((resolve) => {
+      reader.addEventListener("load", () => {
+        const result = new Buffer(reader.result, "binary");
+        resolve(result);
+      });
+      reader.readAsBinaryString(file);
+    });
+  } else {
+    // recover from preview
+    // const commaIdx = dataUrl.indexOf(',');
+    // dataBs64 = dataUrl.substring(commaIdx + 1);
+    // data = new Buffer(dataBs64, 'base64');
+  }
+
+  const prefix = new Buffer("ImageSigningChallenge");
+  const buf = Buffer.concat([prefix, data]);
+  // const bufSha = hash.sha256(buf);
+
+  const formData = new FormData();
+  if (file) {
+    formData.append("file", file);
+  } else {
+    // formData.append('file', file, filename) <- Failed to add filename=xxx to Content-Disposition
+    // Can't easily make this look like a file so this relies on the server supporting: filename and filebinary
+    // formData.append('filename', filename);
+    // formData.append('filebase64', dataBs64);
+  }
+
+  let sig;
+  // if (keychainLogin) {
+  if (true) {
+    const response = await new Promise((resolve) => {
+      window.steem_keychain.requestSignBuffer(username, JSON.stringify(buf), "Posting", (response) => {
+        resolve(response);
+      });
+    });
+    if (response.success) {
+      sig = response.result;
+    } else {
+      console.log("progress", response);
+      // progress({ error: response.message });
+      return;
+    }
+  } else {
+    // sig = Signature.signBufferSha256(bufSha, d).toHex();
+  }
+
+  const uploadImageUrl = "https://steemitimages.com";
+
+  const postUrl = `${uploadImageUrl}/${username}/${sig}`;
+
+  const result = await axios
+    .post(postUrl, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     })
-    .then((r) => r.data);
+    .then((r) => {
+      console.log(r);
+      return r.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log("result", result);
+
+  return result;
 };
 
 export const getMarketData = (
