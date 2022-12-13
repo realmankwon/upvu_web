419cfcb934fb09e9eca1dad3a70f931455172aa4
Suncheol Kwon <elytselyts@gmail.com>
2022-12-13 20:31:59 +0900

    add bug report

--- src/common/components/left-side-area/index.tsx
+++ src/common/components/left-side-area/index.tsx
@@ -21,12 +21,27 @@ export class LeftSideArea extends Component<Props> {
   render() {
     // const { trendingTags, global } = this.props;
     const feedback = require("../../img/feedback.png");
+    const bugreport = require("../../img/bug_report.png");
 
     return (
       <div className="trending-tags-card">
-        <a href="https://9au559zzo9w.typeform.com/to/HU37OEka" target={"_blank"}>
-          <img className="link-card" src={feedback} />
-        </a>
+        <img className="link-card" src={feedback} />
+        <div className="link-text">
+          <p>
+            <a href="https://9au559zzo9w.typeform.com/to/HU37OEka" target={"_blank"}>
+              Feedback
+            </a>
+          </p>
+        </div>
+
+        <img className="link-card" src={bugreport} />
+        <div className="link-text">
+          <p>
+            <a href="https://9au559zzo9w.typeform.com/to/maOGwIiq" target={"_blank"}>
+              Bug Report
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
