75ccff5ab60ee623a26b787af77e9cf7f443d4d0
realmankwon <realman0214@gmail.com>
2023-03-04 19:06:19 +0900

    login error message modified

--- src/common/components/login/index.tsx
+++ src/common/components/login/index.tsx
@@ -136,8 +136,9 @@ export class LoginKc extends BaseComponent<LoginKcProps, LoginKcState> {
       .then(() => {
         this.hide();
       })
-      .catch(() => {
-        error(_t("g.server-error"));
+      .catch((e) => {
+        // error(_t("g.server-error"));
+        error(e.response.data.message);
       })
       .finally(() => {
         this.stateSet({ inProgress: false });
@@ -307,8 +308,9 @@ export class Login extends BaseComponent<LoginProps, State> {
           ls.set(`${user.username}HadTutorial`, "false");
         }
       })
-      .catch(() => {
-        error(_t("g.server-error"));
+      .catch((e) => {
+        // error(_t("g.server-error"));
+        error(e.response.data.message);
       })
       .finally(() => {
         this.stateSet({ inProgress: false });
@@ -474,8 +476,9 @@ export class Login extends BaseComponent<LoginProps, State> {
         }
         this.hide();
       })
-      .catch(() => {
-        error(_t("g.server-error"));
+      .catch((e) => {
+        // error(_t("g.server-error"));
+        error(e.response.data.message);
       })
       .finally(() => {
         this.stateSet({ inProgress: false });
